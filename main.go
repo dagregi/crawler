b049d2e075ae7fb888d5229a5b9d217f1223a73b
dagregi <dagim6066@gmail.com>
2024-08-21 13:09:02 +0300

    basic command line arguments

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Web Crawler in Go")
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else if len(os.Args) == 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	baseURL := os.Args[1]
+	fmt.Printf("starting crawl of: %s...\n", baseURL)
 }
